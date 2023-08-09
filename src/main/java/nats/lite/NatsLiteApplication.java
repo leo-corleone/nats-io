b5525f3c481cdf814d471585225492a274a718d2
suen <1499718389@qq.com>
2023-08-09 22:40:24 +0800

    rename NatsLiteApplication.java

--- src/main/java/nats/lite/NatsLiteApplication.java
+++ src/main/java/nats/lite/NatsLiteApplication.java
@@ -1,10 +1,8 @@
 package nats.lite;
 
 import nats.lite.annotation.EnableNatsDiscovery;
-import nats.lite.example.service.NatsLiteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 /**
@@ -15,11 +13,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableNatsDiscovery(basePackages = "nats.lite.example.api")
 @SpringBootApplication
-public class App {
+public class NatsLiteApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(NatsLiteApplication.class, args);
         System.out.println("Nats lite finish to startup");
     }
 
